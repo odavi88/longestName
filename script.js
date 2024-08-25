2e3fec6f1355f6f60474b1f7d064d6fa107a73f6
Omar Davidson <omardavidson227@gmail.com>
2024-08-25 16:29:55 -0400

    created if statement that handle each case depending on the length of each name

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-let name1 = ""
-let name2 = ""
-let name3 = ""
+let name1 = "";
+let name2 = "";
+let name3 = "";
+
+name1 = prompt("Give me a name...");
+name2 = prompt("Give me another name...");
+name3 = prompt("Give me one more name...");
+
+if (name1.length > name2.length && name1.length > name3.length) { // name1 is the longest
+    console.log(`The longest name is ${name1}.`);
+} else if (name2.length > name1.length && name2.length > name3.length) { // name2 is the longest
+    console.log(`The longest name is ${name2}.`);
+} else if (name3.length > name1.length && name3.length > name2.length) { // name3 is the longest
+    console.log(`The longest name is ${name3}.`);
+} else if (name1.length === name2.length && name3.length < name1.length) { // name1 & name2 are the longest
+    console.log(`${name1} and ${name2} are the longest names.`);
+} else if (name1.length === name3.length && name2.length < name1.length) { // name1 & name3 are the longest
+    console.log(`${name1} and ${name3} are the longest names.`);
+} else if (name2.length === name3.length && name1.length < name2.length) { // name2 & name3 are the longest
+    console.log(`${name2} and ${name3} are the longest names.`);
+} else if (name1.length === name2.length && name2.length === name3.length) { // name1, name2 and name3 are all the same size in length
+    console.log(`${name1}, ${name2} and ${name3} are all the same in length.`);
+} else {
+    console.log("Not a valid input..."); // catch all for errors...
+} 
+
