8e007e386310cf30e887a956a3d4a75103226083
Omar Davidson <omardavidson227@gmail.com>
2024-08-25 16:35:07 -0400

    making final corrections and tweaks to code; submitting

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ name2 = prompt("Give me another name...");
 name3 = prompt("Give me one more name...");
 
 if (name1.length > name2.length && name1.length > name3.length) { // name1 is the longest
-    console.log(`The longest name is ${name1}.`);
+    console.log(`${name1} has the longest name is.`);
 } else if (name2.length > name1.length && name2.length > name3.length) { // name2 is the longest
-    console.log(`The longest name is ${name2}.`);
+    console.log(`${name2} has the longest name is.`);
 } else if (name3.length > name1.length && name3.length > name2.length) { // name3 is the longest
-    console.log(`The longest name is ${name3}.`);
+    console.log(`${name3} has the longest name is.`);
 } else if (name1.length === name2.length && name3.length < name1.length) { // name1 & name2 are the longest
-    console.log(`${name1} and ${name2} are the longest names.`);
+    console.log(`${name1} and ${name2} are tied for the longest names.`);
 } else if (name1.length === name3.length && name2.length < name1.length) { // name1 & name3 are the longest
-    console.log(`${name1} and ${name3} are the longest names.`);
+    console.log(`${name1} and ${name3} are tied for the longest names.`);
 } else if (name2.length === name3.length && name1.length < name2.length) { // name2 & name3 are the longest
-    console.log(`${name2} and ${name3} are the longest names.`);
+    console.log(`${name2} and ${name3} are tied for the longest names.`);
 } else if (name1.length === name2.length && name2.length === name3.length) { // name1, name2 and name3 are all the same size in length
     console.log(`${name1}, ${name2} and ${name3} are all the same in length.`);
 } else {
